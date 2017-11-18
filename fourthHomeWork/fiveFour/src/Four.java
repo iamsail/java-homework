08f4dc40d8d2ec1de8d2d3d7c82e9b46c4514172
Sail <865605793@qq.com>
2017-11-18 12:00:56 +0800

    作业4.4

---
+++ fourthHomeWork/fiveFour/src/Four.java
@@ -0,0 +1,23 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String args[]){
+        Calendar calendar = Calendar.getInstance();
+        Scanner in = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            int a = (int)(Math.random() * 50);
+            int minutes = calendar.get(Calendar.MINUTE);
+            int seconds = calendar.get(Calendar.SECOND);
+d            int b = (int)((seconds + minutes) / 2.4);
+            System.out.println(a+"+"+b+"=");
+            int sum;
+            sum = in.nextInt();
+            if(sum == a + b){
+                System.out.println("对");
+            }else {
+                System.out.println("错");
+            }
+        }
+    }
+}
